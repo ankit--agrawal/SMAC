ebbaf7a5be859397d6a949fb11a789c05a76e974
Ankit Agrawal <ankit@cs.utah.edu>
2017-11-23 15:26:11 -0700

    updating filesSmac

---
+++ example_scenarios/filesSmac/unreach/coresUnreach_16.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+<<<<<<< HEAD
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 1 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 2 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 3 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 4 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 5 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 6 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 7 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 8 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 9 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 10 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 11 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 12 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 13 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 14 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 15 --validation false &
+. ../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 16 --validation false
+=======
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 1 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 2 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 3 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 4 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 5 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 6 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 7 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 8 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 9 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 10 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 11 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 12 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 13 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 14 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 15 --validation false &
+./../../../smac --shared-model-mode true --scenario-file smack-scenario_unreach.txt --seed 16 --validation false
+>>>>>>> 1ae9e022cf2f4dc1a8fce6b88f254df26a8ddac4
