8a2aaa53f0e7b56884becfed152ab40065272b90
Ankit-Lab-VB <ankit@cs.utah.edu>
2018-02-02 18:31:17 -0700

    adding instances of SMAC-files

---
+++ coresLDV_16_2.sh
@@ -0,0 +1,17 @@
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 17 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 18 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 19 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 20 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 21 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 22 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 23 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 24 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 25 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 26 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 27 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 28 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 29 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 30 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 31 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 32 -- validate false
+
---
+++ coresLDV_16_3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 33 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 34 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 35 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 36 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 37 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 38 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 39 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 40 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 41 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 42 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 43 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 44 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 45 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 46 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 47 --validation false &
+./smac --shared-model-mode true --scenario-file example_scenarios/filesSmac/ldv/smack-scenario_ldv.txt --seed 48 --validation false
