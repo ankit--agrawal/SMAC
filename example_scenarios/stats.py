67704eb7a0c4ac6f8f182669846f4b4e259589ae
ankit <ankit@cs.utah.edu>
2018-04-11 18:03:16 -0600

    adding the traj-run* files

--- example_scenarios/stats.py
+++ example_scenarios/stats.py
@@ -1,14 +1,15 @@
 import os,sys,glob
 
-pathname = '/var/tmp/smac-output/**'
+pathname = '/proj/SMACK/smac/example_scenarios/ldv/smac-output/**'
 files = glob.glob(pathname+'/traj-run*.txt')
-#files.sort();
+
 with open("outBestSMACK.txt","w") as f:
 	for i in range(1,len(files)+1):
 		#index = files.index('*'+str(i)+'.txt')
 		g = open(files[i],'r')
-		tmp = g.readlines()
-		print tmp
+		tmp = []
+		for line in g.splitlines():
+			tmp.append(line)
 		g.close()
 		#f.write(tmp[-1]+'\n')
 
