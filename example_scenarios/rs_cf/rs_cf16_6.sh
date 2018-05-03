e57fb4191113d79640f0136223e6b99d86892e53
root <root@bench10.smackbench10.smack.emulab.net>
2018-05-03 09:42:06 -0600

    trying new benchmarks

---
+++ example_scenarios/rs_cf/rs_cf16_6.sh
@@ -0,0 +1,16 @@
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 97 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 98 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 99 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 100 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 101 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 102 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 103 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 104 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 105 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 106 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 107 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 108 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 109 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 110 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 111 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 112 --validation false
---
+++ example_scenarios/rs_cf/rs_cf16_7.sh
@@ -0,0 +1,16 @@
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 113 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 114 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 115 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 116 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 117 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 118 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 119 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 120 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 121 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 122 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 123 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 124 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 125 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 126 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 127 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_rs_cf.txt --seed 128 --validation false
