1ab6aeb070f99261d86374497864969f86abda42
ankit <ankit@cs.utah.edu>
2018-05-09 11:05:27 -0600

    reformatting filenames

---
+++ example_scenarios/ldv/ldv16_8.sh
@@ -0,0 +1,16 @@
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 129 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 130 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 131 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 132 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 133 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 134 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 135 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 136 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 137 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 138 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 139 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 140 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 141 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 142 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 143 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_ldv.txt --seed 144 --validation false
