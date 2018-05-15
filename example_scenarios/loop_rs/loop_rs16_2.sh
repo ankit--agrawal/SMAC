7a469dfb4e9890af5b381ece9631cc4e76ae319a
root <root@node-1.spare.smack.emulab.net>
2018-05-15 10:31:43 -0600

    start: seq_rs

---
+++ example_scenarios/loop_rs/loop_rs16_2.sh
@@ -0,0 +1,16 @@
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 33 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 34 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 35 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 36 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 37 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 38 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 39 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 40 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 41 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 42 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 43 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 44 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 45 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 46 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 47 --validation false &
+../../smac --shared-model-mode true --scenario-file smack-scenario_loop_rs.txt --seed 48 --validation false
