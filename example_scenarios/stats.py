fe9a5f7d07027014686351d3f16000f0628ced28
ankit <ankit@cs.utah.edu>
2018-07-06 14:01:51 -0600

    updating stats

--- example_scenarios/stats.py
+++ example_scenarios/stats.py
@@ -1,15 +1,16 @@
+<<<<<<< HEAD
+import os, sys, glob2
+=======
 import os, sys, glob2, operator
+>>>>>>> 59e55f6ccc0ed4812cab406f673ff5a31500fa87
 
 
-def collectBestIncumbents(filename):
-	with open(filename,"w") as f:
-		for i in range(len(files)):
-			with open(files[i],'r') as g: tmp = g.readlines();
-			tmp = [x.strip() for x in tmp]
-			g.close()
-			f.write(files[i] + ', ' +tmp[-1]+'\n')
-
-	f.close()
+def collectBestIncumbents(tmpList):
+    for i in range(len(files)):
+        with open(files[i],'r') as f: tmp = f.readlines();
+        tmp = [x.strip() for x in tmp]
+        tmpList.append(tmp[-1])
+    return tmpList
 
 def collectParameterStats(filename, d):
 	f = open(filename,'r')
@@ -25,6 +26,20 @@ def collectParameterStats(filename, d):
 	f.close()
 	return d
 
+<<<<<<< HEAD
+if __name__ == "__main__":    
+    pathname = '/proj/SMACK/smac/example_scenarios/'
+    benchmark = str(sys.argv[1])
+    path = pathname + benchmark + 'smac-output/**'
+    files = glob2.glob(pathname + '/traj-run*.txt')
+    bestConfig = []
+    bestConfig = collectBestIncumbents(bestConfig)
+    
+    #creating matrix with different configurations 'bag of words NLP problem'
+    from sklearn.feature_extraction.text import CountVectorizer
+    cv = CountVectorizer()
+    X = cv.fit_transform(bestConfig).toarray()
+=======
 if __name__ == "__main__":
 	pathname1 = '/proj/SMACK/smac/example_scenarios/'
 	benchmark = str(sys.argv[1]) #type of benchmark files
@@ -40,3 +55,4 @@ if __name__ == "__main__":
 	for name,value in stats.items():
 		print name, value
 		t.write(name + ' '+ str(value) + '\n')
+>>>>>>> 59e55f6ccc0ed4812cab406f673ff5a31500fa87
